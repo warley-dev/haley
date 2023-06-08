24f3e8c73f5920b21de50f3e8645fe3f1319fcff
haley <warleyhacker@hotmail.com.com>
2023-06-08 02:55:47 -0300

    save

--- app/Controllers/Web/HomeController.php
+++ app/Controllers/Web/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Web;
 
 use App\Controllers\Controller;
+use DateTime;
 use Haley\Collections\Log;
 use Haley\Database\Query\DB;
 
@@ -10,8 +11,13 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $filmes = DB::table('filmes')->limit('15')->all();
-    
+        $filmes = DB::table('filmes')->limit('15')->all();     
+        
+        $array = [15,31,54];
+
+
+        
+        dd(in_array(150,$array));
 
         return view('home', [
             'filmes' => $filmes
--- routes/job.php
+++ routes/job.php
@@ -7,6 +7,18 @@ use Haley\Jobs\Job;
 // --------------------------------------------------------------------------|
 
 Job::namespace('App\Jobs')->name('test')->group(function () {
-    Job::minute(1, 'Test::one')->name('minute');
-    Job::minute(2, 'Um::one')->name('minute');
+    Job::everyMinute(1, 'Test::one')->description('every 1 minute');
+    Job::everyMinute(6, 'Um::one')->description('every 6 minute');
+
+    Job::everyHour(3, 'Test::one')->description('every hours');
+
+    Job::everyDayAt('01:22')->description('every dat at');
+
+    Job::thursdaysAt('02:18')->description('Quinta-feira');
+
+    Job::raw([02, 03], date('H'))->description('raw');
+
+    Job::dayAt(8, '02:48')->description('day at');
+
+    Job::date('00:05 08/06/2023')->name('date')->description('especific date');
 });
