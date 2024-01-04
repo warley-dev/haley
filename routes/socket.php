78c3107d054be324d3aa359921f1801caa8ec8e0
haley <warleyhacker@hotmail.com.com>
2024-01-03 23:21:47 -0300

    save

--- routes/socket.php
+++ routes/socket.php
@@ -8,4 +8,4 @@ use Haley\Router\Websocket;
 //                            WEBSOCKET ROUTES                               |
 // --------------------------------------------------------------------------|
 
-Websocket::ws(8154, [TesteController::class])->host('ws://codehalley.com')->name('teste');
+Websocket::ws(8154, [TesteController::class])->host('codehalley.com')->name('teste');
