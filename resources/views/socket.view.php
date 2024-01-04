bbef33b6fb7259f7adf06f53d7cdde02d8030479
haley <warleyhacker@hotmail.com.com>
2024-01-03 22:39:53 -0300

    save

--- resources/views/socket.view.php
+++ resources/views/socket.view.php
@@ -75,7 +75,7 @@
 		}
 
 		$(document).ready(function() {
-			var websocket = new WebSocket("ws://localhost:9213");
+			var websocket = new WebSocket("ws://localhost:9214");
 
 			websocket.onopen = function(event) {
 				showMessage("<div class='chat-connection-ack'>Connection is established!</div>");
@@ -101,12 +101,14 @@
 
 				$('#chat-user').attr("type", "hidden");
 				
-				var messageJSON = {
+				var messageJSON = JSON.stringify({
 					chat_user: $('#chat-user').val(),
 					chat_message: $('#chat-message').val()
-				};
+				},null,0);
 
-				websocket.send(JSON.stringify(messageJSON));
+				console.log(messageJSON)
+
+				websocket.send(messageJSON);
 			});
 		});
 	</script>
@@ -115,6 +117,7 @@
 <body>
 	<form name="frmChat" id="frmChat">
 		<div id="chat-box"></div>
+
 		<input type="text" name="chat-user" id="chat-user" placeholder="Name" class="chat-input" required />
 		<input type="text" name="chat-message" id="chat-message" placeholder="Message" class="chat-input chat-message" required />
 		<input type="submit" id="btnSend" name="send-chat-message" value="Send">
