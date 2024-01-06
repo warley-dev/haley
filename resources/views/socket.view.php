b51387e6066010948b698f639f4a8e9594a3be3f
haley <warleyhacker@hotmail.com.com>
2024-01-06 13:38:45 -0300

    save

--- resources/views/socket.view.php
+++ resources/views/socket.view.php
@@ -95,7 +95,7 @@
 		}
 
 		setTimeout(() => {
-			var websocket = new WebSocket("ws://localhost:9259");
+			var websocket = new WebSocket("ws://localhost:9250");
 
 			websocket.onopen = function(event) {				
 				// console.log(event);
