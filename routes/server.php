3619ae4b5d5674d11eeaa12db2b1e4ba75b23394
warley rodrigues <haleydev@hotmail.com>
2024-06-09 07:30:00 -0300

    save

--- routes/server.php
+++ routes/server.php
@@ -7,10 +7,10 @@ use Haley\Server\Server;
 // --------------------------------------------------------------------------|
 
 try {
-    Server::name('server')->group(function () {
+    // Server::name('server')->group(function () {
         Server::ws('framework', 5005, 'App\Controllers\Server\Teste')->path('helo/{dois}/{tres}/{quatro?}')->name('1');
         // Server::ws('framework', 6000, 'Teste')->name('server-2');
-    });;
+    // });
 } catch (\Throwable $th) {
     dd($th);
 }
