242f4d4ab89688c1ca0816230309e34d434aa278
warley rodrigues <haleydev@hotmail.com>
2024-06-09 07:30:39 -0300

    save

--- routes/server.php
+++ routes/server.php
@@ -6,14 +6,11 @@ use Haley\Server\Server;
 //                           SERVERS ROUTES                                  |
 // --------------------------------------------------------------------------|
 
-try {
-    // Server::name('server')->group(function () {
-        Server::ws('framework', 5005, 'App\Controllers\Server\Teste')->path('helo/{dois}/{tres}/{quatro?}')->name('1');
-        // Server::ws('framework', 6000, 'Teste')->name('server-2');
-    // });
-} catch (\Throwable $th) {
-    dd($th);
-}
+Server::namespace('App\Controllers\Server')->name('server')->group(function () {
+    Server::ws('framework', 5005, 'Teste')->path('helo/{dois}/{tres}/{quatro?}')->name('1');
+    // Server::ws('framework', 6000, 'Teste')->name('server-2');
+});
+
 
 
 // rename channels
